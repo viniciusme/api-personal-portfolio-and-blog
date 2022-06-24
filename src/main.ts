8eb03cff357320a3e0038fb9817d2037108ae94f
Vinicius Mendes <41807765+viniciusme@users.noreply.github.com>
2022-06-24 19:10:06 -0300

    feat: add dto para posts e suas validações

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,5 @@
 import { NestFactory } from '@nestjs/core';
-import { Logger } from '@nestjs/common';
+import { Logger, ValidationPipe } from '@nestjs/common';
 import { AppModule } from './app.module';
 
 const port = 3000;
@@ -7,6 +7,12 @@ const port = 3000;
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const logger = new Logger();
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+    }),
+  );
 
   await app.listen(port);
   logger.log(`<< Servidor rodando na porta: ${await app.getUrl()} >>`);
--- src/post/dtos/create-post.dto.ts
+++ src/post/dtos/create-post.dto.ts
@@ -1,11 +1,33 @@
+import { IsArray, IsBoolean, IsNotEmpty, IsString } from 'class-validator';
 import { PostCategory } from '../enums';
 
 export class CreatePostDto {
-  'title': string;
-  'content': string;
-  'slug': string;
-  'excerpt': string;
-  'category': PostCategory;
-  'tags': string[];
+  @IsString()
+  @IsNotEmpty()
+  'title': string; // É o título do Post
+
+  @IsString()
+  @IsNotEmpty()
+  'content': string; // É Conteúdo do Post
+
+  @IsString()
+  @IsNotEmpty()
+  'slug': string; // É o texto que aparece logo após o seu domínio como parte do link permanente que leva ate o conteúdo.
+
+  @IsString()
+  @IsNotEmpty()
+  'excerpt': string; // É utilizado para fazer a descrição do conteúdo do Post
+
+  @IsString()
+  @IsNotEmpty()
+  'category': PostCategory; // É utilizado para criar diferentes áreas de um Blog ou conteúdo, organizando assuntos em suas devidas categorias
+
+  @IsArray()
+  @IsString({ each: true })
+  @IsNotEmpty()
+  'tags': string[]; // As tags servem para ajudar e complementar na organização das categorias do seu conteúdo, seria a classificação dos detalhes.
+
+  @IsBoolean()
+  @IsNotEmpty()
   'status': boolean;
 }
