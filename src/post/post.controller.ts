ad8706dff70add9fd42de8cd312d2ddc1e44af8d
Vinicius Mendes <vini@vinimendes.com.br>
2022-06-24 12:49:21 -0300

    feat: add endpoints da rota posts

--- src/post/post.controller.ts
+++ src/post/post.controller.ts
@@ -1,4 +1,37 @@
-import { Controller } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Post,
+  Put,
+} from '@nestjs/common';
 
-@Controller('post')
-export class PostController {}
+@Controller('posts')
+export class PostController {
+  @Get()
+  getMany() {
+    return {
+      message: 'Rota funcionando!',
+    };
+  }
+
+  @Get(':id')
+  getOne(@Param('id') id: string) {
+    return {
+      message: 'getOne',
+    };
+  }
+
+  @Post()
+  createOne(@Body('title') title: any) {
+    console.log(title);
+  }
+
+  @Put(':id')
+  editOne(@Param('id') id: string) {}
+
+  @Delete(':id')
+  deleteOne(@Param('id') id: string) {}
+}
