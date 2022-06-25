e7e5442febe85d36ae858e4c94899019cecb215b
Vinicius Mendes <41807765+viniciusme@users.noreply.github.com>
2022-06-24 23:27:11 -0300

    Feat: add typorm, implementado e configurado o docker-composer

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,23 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { PostModule } from './post/post.module';
 
 @Module({
-  imports: [PostModule],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: 'localhost',
+      port: 3306,
+      username: 'myblog_user',
+      password: 'myblog_password',
+      database: 'portfolio_and_blog',
+      entities: [__dirname + './**/**/*entity{.ts,.js'],
+      synchronize: true,
+    }),
+    PostModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
