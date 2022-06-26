75f5a1a25f7fab12a73c7d49a9b10e9f5a6a9810
Vinicius Mendes <41807765+viniciusme@users.noreply.github.com>
2022-06-26 17:54:35 -0300

    Feat: criado as configurações da entidade do usuário

--- src/user/entities/user.entity.ts
+++ src/user/entities/user.entity.ts
@@ -1,1 +1,43 @@
-export class UserEntity {}
+import {
+  BeforeInsert,
+  BeforeUpdate,
+  Column,
+  CreateDateColumn,
+  Entity,
+  PrimaryGeneratedColumn,
+} from 'typeorm';
+import { hash } from 'bcryptjs';
+
+@Entity('users')
+export class User {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column({ type: 'varchar', length: 255 })
+  name: string;
+
+  @Column({ name: 'last_name', type: 'varchar', length: 255 })
+  lastName: string;
+
+  @Column({ type: 'varchar', length: 255, nullable: false })
+  email: string;
+
+  @Column({ type: 'varchar', length: 128, nullable: false, select: false })
+  password: string;
+
+  @Column({ type: 'bool', default: true })
+  status: boolean;
+
+  @CreateDateColumn({ name: 'created_at', type: 'timestamp' })
+  createdAt: Date;
+
+  //   @BeforeInsert()
+  //   @BeforeUpdate()
+  //   async hashPassword() {
+  //     if (!this.password) {
+  //       return;
+  //     }
+
+  //     this.password = await hash(this.password, 10);
+  //   }
+}
