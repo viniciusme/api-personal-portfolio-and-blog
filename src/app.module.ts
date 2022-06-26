3b541e156973a3feb403feb9a522b9eb1060ee8e
Vinicius Mendes <41807765+viniciusme@users.noreply.github.com>
2022-06-26 15:53:07 -0300

    Feat: add creado estrutura do modulo user

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,6 +4,7 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { PostModule } from './post/post.module';
+import { UserModule } from './user/user.module';
 
 @Module({
   imports: [
@@ -19,6 +20,7 @@ import { PostModule } from './post/post.module';
       synchronize: true,
     }),
     PostModule,
+    UserModule,
   ],
   controllers: [AppController],
   providers: [AppService],
