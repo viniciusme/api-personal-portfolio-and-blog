bdf4ad61651ddb73e67556ce87bbde840dce68da
Vinicius Mendes <41807765+viniciusme@users.noreply.github.com>
2022-06-26 18:48:46 -0300

    Feat: Implementado os metodos do user controller e user service

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,4 +1,26 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Delete, Get, Post, Put } from '@nestjs/common';
+import { UserService } from './user.service';
 
 @Controller('user')
-export class UserController {}
+export class UserController {
+  constructor(private readonly userService: UserService) {}
+
+  @Get()
+  async getMany() {
+    return await this.userService.getMany();
+  }
+
+  @Get(':id')
+  async getOne() {
+    // return await this.userService.getOne();
+  }
+
+  @Post()
+  createOne() {}
+
+  @Put(':id')
+  editOne() {}
+
+  @Delete(':id')
+  delete() {}
+}
--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,4 +1,35 @@
-import { Injectable } from '@nestjs/common';
+import { CreateUserDto } from './dtos/create-user.dto';
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { User } from './entities/user.entity';
 
 @Injectable()
-export class UserService {}
+export class UserService {
+  constructor(
+    @InjectRepository(User)
+    private readonly userRepository: Repository<User>,
+  ) {}
+
+  async getMany() {
+    return await this.userRepository.find();
+  }
+
+  async getOne(id: number) {
+    const user = await this.userRepository.findOne({ where: { id } });
+
+    if (!user) throw new NotFoundException('Usuário não existe.');
+
+    return user;
+  }
+
+  async createOne(dto: CreateUserDto) {
+    const newUser = this.userRepository.create(dto);
+
+    return await this.userRepository.save(newUser);
+  }
+
+  async editOne() {}
+
+  async deleteOne() {}
+}
